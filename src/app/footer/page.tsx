6d262f32aa33b1e53d97b6100cdb506d71c942a6
Dhiraj-Striveup <dhiraj.striveup@gmail.com>
2025-10-02 12:48:05 +0530

    Update page.tsx

--- src/app/footer/page.tsx
+++ src/app/footer/page.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-black text-gray-400 p-6 text-center flex  items-center md:flex-col flex-col lg:flex-row justify-center  lg:justify-between ">
+        <footer className="bg-black text-gray-400 p-6 text-center flex  items-center md:flex-col flex-col lg:flex-row justify-center  lg:justify-center ">
             <div className="flex justify-center">
             <p  className="text-white">Created by @Dhiraj</p>
             </div>
